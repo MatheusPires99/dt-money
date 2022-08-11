39e48f2f91439d2d2cc87c39e46f5b175a3a83af
Matheus Pires <matheushenriquepires99@gmail.com>
2022-08-11 11:30:36 -0300

    feat: Create Summary component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/header";
+import { Summary } from "../../components/summary";
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 };

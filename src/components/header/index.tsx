8a50f4b9ddbca28af7296ad3276d28c274a39b17
Matheus Pires <matheushenriquepires99@gmail.com>
2022-08-11 13:57:18 -0300

    feat: Create TransactionsModal component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,7 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
 import dtMoneyLogo from "../../assets/dt-money-logo.svg";
+import { NewTranscationModal } from "../new-transaction-modal";
 import { HeaderContainer, HeaderContent, NewTranscationButton } from "./styles";
 
 export const Header = () => {
@@ -6,7 +9,14 @@ export const Header = () => {
     <HeaderContainer>
       <HeaderContent>
         <img src={dtMoneyLogo} alt="" />
-        <NewTranscationButton>Nova transação</NewTranscationButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTranscationButton>Nova transação</NewTranscationButton>
+          </Dialog.Trigger>
+
+          <NewTranscationModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
